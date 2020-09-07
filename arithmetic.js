ee62cb2383586be53dc8a0643fe931722a129f56
Your Name <you@example.com>
2020-09-07 23:32:06 +0800

    a

--- arithmetic.js
+++ arithmetic.js
@@ -1,15 +1,57 @@
 
 /** --------------------------------------------------------------------排序------------------------------------------------------------------- */
 
-// 冒泡排序
+// 冒泡排序 (较大的一个一个冒泡到后面)
 function bubleSort(arr) {
   for( let outer = arr.length; outer >= 2; outer-- ) {
     for(let inner = 0; inner < outer -1 ; inner++) {
       if(arr[inner] > arr[inner+1]) {
-        [arr[inner], arr[inner + 1]] = [arr[inner + 1], arr]
+        [arr[inner], arr[inner + 1]] = [arr[inner + 1], arr[inner]]
       }
     }
   }
+  return arr
 }
 
-// 选择排序
+// 选择排序 (较小的一个一个比较到前面)
+function selectSort(arr) {
+  
+  for (let outer = 0; outer < arr.length - 1; outer++ ) {
+    for (let inner = outer + 1; inner < arr.length; inner++ ) {
+      if(arr[outer] > arr[inner]) {
+        [ arr[outer], arr[inner] ] = [ arr[inner], arr[outer] ]
+      }
+    }
+  }
+  
+  return arr
+}
+
+// 插入排序 (以第一个为基准 遍历后面，并与前面比较，小则插入到前面)
+function insertSort(arr) {
+
+  for(let i = 1; i < arr.length; i++  ) {
+    for(let j = i; j > 0; j--) {
+      if(arr[j] < arr[j-1]) {
+        [ arr[j], arr[j-1] ] = [ arr[j-1], arr[j] ]
+      } else {
+        break;
+      }
+    }
+  }
+  return arr
+}
+
+// 快速排序
+
+
+
+
+
+
+
+
+
+let a = [2, 5,3,7,4,9,12,4,56,77]
+// console.log(bubleSort(a))
+// console.log(selectSort(a))
